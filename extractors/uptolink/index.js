81f536bd3938f88c3aeaac2cf9ceee37bef904c2
aria <mrsariablue@pm.me>
2022-01-22 19:01:20 -0500

    add uptolink1.com/uptoolink.org completely

---
+++ extractors/uptolink/index.js
@@ -0,0 +1,27 @@
+const got = require("got");
+const cheerio = require("cheerio");
+
+exports.bypass = function(url, cb) {
+  got(url, {
+    headers: {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+      "Accept-Language": "en-US,en;q=0.5",
+      "Accept-Encoding": "gzip, deflate, br",
+      "DNT": "1",
+      "Connection": "keep-alive",
+      "Upgrade-Insecure-Requests": "1"
+    }
+  }).then(function(resp) {
+    var $ = cheerio.load(resp.body);
+    var l = [];
+    for (var c in $("a[rel=nofollow]")) {
+      if ($("a[rel=nofollow]")[c] !== undefined && $("a[rel=nofollow]")[c].attribs !== undefined) {
+        l.push($("a[rel=nofollow]")[c].attribs.href)
+      }
+    }
+    cb(null, l);
+  }).catch(function(err) {
+    cb(err, null);
+  });
+}

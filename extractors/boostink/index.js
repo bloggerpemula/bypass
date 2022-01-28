ac51a76642dbf9c3259bf191e15cc291948df2ef
aria <mrsariablue@pm.me>
2022-01-27 21:48:52 -0500

    fix + futureproof boost.ink

--- extractors/boostink/index.js
+++ extractors/boostink/index.js
@@ -14,17 +14,30 @@ exports.bypass = function(url, cb) {
     }
   }).then(function(resp) {
     var $ = cheerio.load(resp.body);
-    // sorry for party rockin
-    // in all seriousness, i wouldn't have a problem with your site enough to make this if you didn't use
-    // invasive popup ads on your site, or make it possible to use this site without javascript.
-    // idk if you even mean mine or universal bypass tbh
     for (var c in $("script")) {
       if (
         $("script")[c] !== undefined &&
         $("script")[c].attribs !== undefined && 
-        $("script")[c].attribs["kekw"] !== undefined
+        $("script")[c].attribs["src"] !== undefined &&
+        $("script")[c].attribs["src"].includes("assets/js/unlock.js")
       ) { 
-        cb(null, Buffer.from($("script")[c].attribs["kekw"], "base64").toString("ascii"));
+        var a = $("script")[c].attribs;
+        got(`https://boost.ink${a["src"]}`, {
+          headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br",
+            "DNT": "1",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1"
+          }
+        }).then(function(res) {
+          var attr = res.body.split(`document.currentScript.removeAttribute("`)[1].split(`");`)[0];
+          if (!a[attr]) {cb("Boost.ink script is broken. Please report this issue.", null);}
+          var l = Buffer.from(a[attr], "base64").toString("ascii");
+          cb(null, l);
+        })
       } else {
         continue;
       }

86244223e400474f747512be015692cf82a25ceb
aria <mrsariablue@pm.me>
2022-02-02 21:06:56 -0500

    improve bypasses

--- extractors/gamesdrive/index.js
+++ extractors/gamesdrive/index.js
@@ -1,3 +1,11 @@
 exports.bypass = function(url, cb) {
-  cb(null, Buffer.from(url.split("/#/link/")[1].split(".")[0], "base64").toString("ascii"));
+  try {
+    if (url.split("/#/link/").length > 1) {
+      cb(null, Buffer.from(url.split("/#/link/")[1].split(".")[0], "base64").toString("ascii"));
+    } else {
+      cb("No redirects found", null);
+    }
+  } catch(e) {
+    cb(e, null);
+  }
 }
